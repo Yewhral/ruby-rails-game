b5ad45c9dc2390b7f65ed88b6a5750666d66f0d6
Michał <michal.zabrzyjewski@theleasy.com>
2018-02-11 20:39:46 +0100

    Hello RoR and setup for deployment on Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: 'Hey! Maybe I will like RoR enough to become a Rails developer? :D'
+  end
 end
