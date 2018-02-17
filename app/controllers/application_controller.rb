71f19d6d52b76559c959042a46a2564a0b0ecaf2
Yewhral <zabrzyjewski@gmail.com>
2018-02-17 20:36:19 +0100

    Log in with session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
 
-  def hello
-    render html: 'Hey! Maybe I will like RoR enough to become a Rails developer? :D'
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
 
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :users
 
 end
