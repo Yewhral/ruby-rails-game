837141b86befaace560ff6dfb1166e89af7d10bb
Yewhral <zabrzyjewski@gmail.com>
2018-03-02 09:05:18 +0100

    Rails in html conditioning

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -5,5 +5,22 @@ class NoticesController < ApplicationController
     @notices = Notice.paginate(page: params[:page], :per_page => 6)
   end
 
+  def create
+    @notice = current_user.notices.build(notice_params)
+    if @notice.save
+      flash[:success] = "Message posted!"
+    end
+    redirect_to root_url
+  end
+
+  def destroy
+  end
+
+  private
+
+  def notice_params
+    params.require(:notice).permit(:content)
+  end
+
 
 end
