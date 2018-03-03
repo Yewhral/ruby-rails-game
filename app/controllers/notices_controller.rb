17bc434416d7b00bef42c87c0f225e6d54180fbe
Yewhral <zabrzyjewski@gmail.com>
2018-03-03 13:17:05 +0100

    Admin and post owner can delete posts

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,5 +1,6 @@
 class NoticesController < ApplicationController
   before_action :logged_in_user
+  before_action :admin_or_owner, only: :destroy
 
 
   def index
@@ -8,6 +9,7 @@ class NoticesController < ApplicationController
   end
 
   def create
+    @notices = Notice.paginate(page: params[:page], :per_page => 6)
     @notice = current_user.notices.build(notices_params)
     if @notice.save
       flash[:success] = "Message posted!"
@@ -18,14 +20,24 @@ class NoticesController < ApplicationController
   end
 
   def destroy
-
+    @notice.destroy
+    flash.now[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
+
   def notices_params
     params.require(:notice).permit(:content, :title)
   end
 
-
+  def admin_or_owner
+    @notice = current_user.notices.find_by(id: params[:id])
+    if current_user.admin?
+      @notice = Notice.find_by(id: params[:id])
+    elsif @notice.nil?
+      redirect_to(root_url)
+    end
+  end
 
 end
